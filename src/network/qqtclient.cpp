5db7c22365c71ebfe67ba0618f655500132ec73f
tianduanrui <2407223896@qq.com>
2017-11-18 21:15:29 +0800

    update

--- src/network/qqtclient.cpp
+++ src/network/qqtclient.cpp
@@ -1,39 +1,40 @@
+#include "qqtclient.h"
+
+#include <QTcpSocket>
+#include <QHostInfo>
+
 #include "qqtnetwork.h"
 #include "qqtcore.h"
 #include "qqt.h"
 
-#include <QTcpSocket>
-#include <QHostInfo>
-#include "qqtclient.h"
-
-QQTClient::QQTClient(QObject* parent) :
-    QTcpSocket(parent)
+QQTClient::QQTClient ( QObject* parent ) :
+    QTcpSocket ( parent )
 {
-    connect(this, SIGNAL(stateChanged(QAbstractSocket::SocketState)), this,
-            SLOT(socketStateChanged(QAbstractSocket::SocketState)));
+    connect ( this, SIGNAL ( stateChanged ( QAbstractSocket::SocketState ) ), this,
+              SLOT ( socketStateChanged ( QAbstractSocket::SocketState ) ) );
     // connected
-    connect(this, SIGNAL(connected()), this, SLOT(socketConnected()));
+    connect ( this, SIGNAL ( connected() ), this, SLOT ( socketConnected() ) );
     // disconnected
-    connect(this, SIGNAL(disconnected()), this, SLOT(socketDisconnect()));
+    connect ( this, SIGNAL ( disconnected() ), this, SLOT ( socketDisconnect() ) );
     // domain
-    connect(this, SIGNAL(hostFound()), this, SLOT(domainHostFound()));
+    connect ( this, SIGNAL ( hostFound() ), this, SLOT ( domainHostFound() ) );
     // error
-    connect(this, SIGNAL(error(QAbstractSocket::SocketError)), this,
-            SLOT(socketErrorOccured(QAbstractSocket::SocketError)));
+    connect ( this, SIGNAL ( error ( QAbstractSocket::SocketError ) ), this,
+              SLOT ( socketErrorOccured ( QAbstractSocket::SocketError ) ) );
 
-    connect(this, SIGNAL(readyRead()), this, SLOT(readyReadData()));
+    connect ( this, SIGNAL ( readyRead() ), this, SLOT ( readyReadData() ) );
 
-    connect(this, SIGNAL(bytesWritten(qint64)), this, SLOT(updateProgress(qint64)));
+    connect ( this, SIGNAL ( bytesWritten ( qint64 ) ), this, SLOT ( updateProgress ( qint64 ) ) );
 
-    connect(this, SIGNAL(bytesWritten(qint64)), this, SIGNAL(signalUpdateProgress(qint64)));
+    connect ( this, SIGNAL ( bytesWritten ( qint64 ) ), this, SIGNAL ( signalUpdateProgress ( qint64 ) ) );
 
     /*
      * 心情很好，但是给自己带来麻烦顾虑，也不能给系统节约多少什么资源, Qt::QueuedConnection);
      */
 
-    setSocketOption(QAbstractSocket::LowDelayOption, 0);
-    setSocketOption(QAbstractSocket::KeepAliveOption, 0);
-    setReadBufferSize(_TCP_RECVBUFF_SIZE);
+    setSocketOption ( QAbstractSocket::LowDelayOption, 0 );
+    setSocketOption ( QAbstractSocket::KeepAliveOption, 0 );
+    setReadBufferSize ( _TCP_RECVBUFF_SIZE );
 
     m_PORT = 0;
     /*
@@ -47,25 +48,25 @@ QQTClient::~QQTClient()
 {
 }
 
-void QQTClient::installProtocol(QQtProtocol* stack)
+void QQTClient::installProtocol ( QQtProtocol* stack )
 {
-    if (m_protocol)
+    if ( m_protocol )
         return;
 
     m_protocol = stack;
-    connect(m_protocol, SIGNAL(write(const QByteArray&)),
-            this, SLOT(write(const QByteArray&)));
+    connect ( m_protocol, SIGNAL ( write ( const QByteArray& ) ),
+              this, SLOT ( write ( const QByteArray& ) ) );
 }
 
-void QQTClient::uninstallProtocol(QQtProtocol* stack)
+void QQTClient::uninstallProtocol ( QQtProtocol* stack )
 {
-    Q_UNUSED(stack)
+    Q_UNUSED ( stack )
 
-    if (!m_protocol)
+    if ( !m_protocol )
         return;
 
-    disconnect(m_protocol, SIGNAL(write(const QByteArray&)),
-               this, SLOT(write(const QByteArray&)));
+    disconnect ( m_protocol, SIGNAL ( write ( const QByteArray& ) ),
+                 this, SLOT ( write ( const QByteArray& ) ) );
     m_protocol = NULL;
 }
 
@@ -78,20 +79,20 @@ void QQTClient::SendConnectMessage()
 {
     pline() << isValid() << isOpen() << state();
 
-    if (!isValid() && !isOpen())
+    if ( !isValid() && !isOpen() )
     {
         connectToSingelHost();
         return;
     }
 
-    if (state() == HostLookupState ||
-        state() == ConnectingState)
+    if ( state() == HostLookupState ||
+         state() == ConnectingState )
     {
         emit signalConnecting();
         return;
     }
 
-    if (state() == ConnectedState)
+    if ( state() == ConnectedState )
         emit signalConnectSucc();
 
     return;
@@ -102,12 +103,12 @@ int QQTClient::SendDisConnectFromHost()
 {
     pline() << isValid() << isOpen() << state();
 
-    if (isValid() || isOpen())
+    if ( isValid() || isOpen() )
     {
 #if defined(__WIN32__) || defined (__WIN64__)
         ;
 #else
-        shutdown(this->socketDescriptor(), SHUT_RDWR);
+        shutdown ( this->socketDescriptor(), SHUT_RDWR );
 #endif
         disconnectFromHost();
         waitForDisconnected();
@@ -128,11 +129,11 @@ void QQTClient::domainHostFound()
  * @param eSocketState
  * 状态函数
  */
-void QQTClient::socketStateChanged(QAbstractSocket::SocketState eSocketState)
+void QQTClient::socketStateChanged ( QAbstractSocket::SocketState eSocketState )
 {
     pline() << eSocketState;
 
-    switch (eSocketState)
+    switch ( eSocketState )
     {
     case QAbstractSocket::HostLookupState:
     case QAbstractSocket::ConnectingState:
@@ -158,14 +159,14 @@ void QQTClient::socketStateChanged(QAbstractSocket::SocketState eSocketState)
  * @param e
  * 状态函数
  */
-void QQTClient::socketErrorOccured(QAbstractSocket::SocketError e)
+void QQTClient::socketErrorOccured ( QAbstractSocket::SocketError e )
 {
     /*
      * 在错误状态下重新连接其他热点，直到确定连接类型，写入配置文件
      */
     pline() << e;
 
-    switch (e)
+    switch ( e )
     {
     case QAbstractSocket::RemoteHostClosedError:
         break;
@@ -199,17 +200,17 @@ void QQTClient::socketDisconnect()
     pline();
 }
 
-void QQTClient::updateProgress(qint64 bytes)
+void QQTClient::updateProgress ( qint64 bytes )
 {
-    Q_UNUSED(bytes)
+    Q_UNUSED ( bytes )
     //pline() << bytes;
 }
 
 void QQTClient::connectToSingelHost()
 {
     int contype = eConType % m_serverIP.size();
-    QString ip = m_serverIP.at(contype);
-    connectToHost(QHostAddress(ip), m_PORT);
+    QString ip = m_serverIP.at ( contype );
+    connectToHost ( QHostAddress ( ip ), m_PORT );
 
     pline() << peerName() << m_PORT;
 }
@@ -219,5 +220,5 @@ void QQTClient::readyReadData()
 {
     QByteArray bytes;
     bytes = readAll();
-    m_protocol->translator(bytes);
+    m_protocol->translator ( bytes );
 }
