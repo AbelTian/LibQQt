17196e9956741bb9319b20c4f20cc18f7ffdb978
tianduanrui <tianduanrui@163.com>
2018-05-14 16:45:13 +0800

    update

--- src/exquisite/qqtcustomprogressbar.cpp
+++ src/exquisite/qqtcustomprogressbar.cpp
@@ -1,6 +1,6 @@
-﻿#if _MSC_VER >= 1600
-#pragma execution_character_set("utf-8")
-#endif
+﻿//#if _MSC_VER >= 1600
+//#pragma execution_character_set("utf-8")
+//#endif
 
 #include "qqtcustomprogressbar.h"
 #include "qpainter.h"
