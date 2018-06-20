31a2ca28ac9f10d8396e92f1a3e5613837a7a1c1
tianduanrui <tianduanrui@163.com>
2018-06-20 17:00:53 +0800

    add dict table model

---
+++ src/gui/qqtdicttablemodel.h
@@ -0,0 +1,27 @@
+﻿#ifndef QQTDICTTABLEMODEL_H
+#define QQTDICTTABLEMODEL_H
+
+#include <qqttablemodel.h>
+#include <qqtdictionary.h>
+#include <qqt-local.h>
+
+class QQTSHARED_EXPORT QQtDictTableModel : public QQtTableModel
+{
+    Q_OBJECT
+public:
+    explicit QQtDictTableModel ( QObject* parent = 0 );
+
+
+signals:
+
+public slots:
+
+    // QQtDictTableModel interface
+public:
+
+protected:
+
+private:
+};
+
+#endif // QQTDICTTABLEMODEL_H
