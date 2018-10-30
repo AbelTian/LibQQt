bd17f85c79ba0208ea9bb67d2d642db71815b3a9
tianduanrui <tianduanrui@163.com>
2018-10-30 11:36:13 +0800

    update

--- examples/console_app/main.cpp
+++ examples/console_app/main.cpp
@@ -14,6 +14,11 @@ int main ( int argc, char* argv[] )
     dict[0][1][2][3][4]["my name"][5][6] = "a string";
     pline() << dict[0][1][2][3][4]["my name"][5][6].getValue().toString();
 
+    //win32: 命令行输入 chcp 65501 乱码解除
+#ifdef __WIN__
+    //会导致清屏，请用户手动设置chcp纠正代码页。LibQQt程序一律为UTF-8编码输出。
+    //system("chcp 65001");
+#endif
     QTextStream qin ( stdin );
     pline() << "你的输入：" << qin.readLine();
 
