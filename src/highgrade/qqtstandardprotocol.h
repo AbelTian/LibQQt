d8c4ee93ac2b4d0f2ba60cda94a2b590de45200f
tianduanrui <2407223896@qq.com>
2020-03-21 17:47:44 +0800

    更新标准数据协议

--- src/highgrade/qqtstandardprotocol.h
+++ src/highgrade/qqtstandardprotocol.h
@@ -22,14 +22,25 @@ public:
 
     virtual void sendMessage ( const QQtDataSerialization& data ) {
         //已经序列化好buffer
-        QByteArray bytes = data.buffer();
+#if 0
+        QQtDataSerialization ds0 = data;
+        ds0.serialize(); //数据本地化，补充一次序列化。
+#else
+        const QQtDataSerialization& ds0 = data;
+#endif
+        QByteArray bytes = ds0.buffer();
         write ( bytes );
     }
 
     virtual void recvMessage ( const QQtDataSerialization& data ) {
         //已经反序列化好buffer
+        emit notifyToProtocolManager ( this, &data );
+        emit readyRead ( data );
     }
 
+
+signals:
+    void readyRead ( const QQtDataSerialization& data );
 protected:
 private:
 
