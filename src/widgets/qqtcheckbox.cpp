9d99dbed1379e7c299a60e0aed55dcc9043d0df9
tianduanrui <2407223896@qq.com>
2020-04-27 19:05:57 +0800

    update qqtcheckbox

--- src/widgets/qqtcheckbox.cpp
+++ src/widgets/qqtcheckbox.cpp
@@ -163,6 +163,12 @@ void QQtCheckBox::paintEvent ( QPaintEvent* event )
     //把imageTable()[stat]设置到mImage。
     translateImage();
 
+#if 0
+    //这个代码是测试代码。如果把translateImage放在外边，就需要这个代码，作为对初始化的判断。
+    if ( mImage.isNull() )
+        translateImage();
+#endif
+
     if ( mImage.isNull() )
         return QCheckBox::paintEvent ( event );
 
